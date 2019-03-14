b0ce71f54292e42c3112c1859e5c548c484dfc7a
unknown <ilhamriana3@gmail.com>
2019-03-14 11:54:32 +0700

    update progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,113 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main()=> runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'First App',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter App Example'),
-            backgroundColor: Colors.deepOrange,
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: new LoginPage(),
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      )
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget{
+  @override
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+  @override
+  void initState(){
+    super.initState();
+    _iconAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 500)
+    );
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.easeOut
+    );
+    _iconAnimation.addListener(()=> this.setState((){}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext content){
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+            image: new AssetImage('assets/market.png'),
+            fit: BoxFit.cover,
+            color: Colors.black54,
+            colorBlendMode: BlendMode.darken,
           ),
-          body:  Column(
-              children: <Widget>[
-                Image.network('https://www.petanikode.com/img/flutter/flutter.png'),
-                Text(
-                  'Lorem ipsum dolor sit amet',
-                  style: TextStyle(fontSize: 24, fontFamily: "Serif", height: 2.0),
-                ),
-                Text('by lhmrnfrzrfr'),
-                Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text('Deliver features faster', textAlign: TextAlign.center),
-                  ),
-                  Expanded(
-                    child: Text('Craft beautiful UIs', textAlign: TextAlign.center),
-                  ),
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.contain, // otherwise the logo will be tiny
-                      child: const FlutterLogo(),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 100,
+              ),
+
+              new Form(
+                child: new Theme(
+                  data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: new InputDecorationTheme(
+                      labelStyle: new TextStyle(
+                        color: Colors.teal, fontSize: 20.0
+                      )
+                    )),
+                  child: Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Enter Email",
+
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Enter Password",
+
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                        ),
+                        new MaterialButton(
+                          height: 40.0,
+                          minWidth: 100.0,
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                          child: new Text("Login"),
+                          onPressed: () => {},
+                          splashColor: Colors.black,
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               )
-            ]
+            ],
           )
+        ],
       ),
     );
   }
